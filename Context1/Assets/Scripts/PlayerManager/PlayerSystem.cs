b45ab44175941f1e77d132fb4862a9ffbf62abbb
mandarinCapybara <mitchelgeraedts@gmail.com>
2024-02-24 10:57:16 +0100

    Moved Player system to new folder

--- Context1/Assets/Scripts/PlayerManager/PlayerSystem.cs
+++ Context1/Assets/Scripts/PlayerManager/PlayerSystem.cs
@@ -7,6 +7,22 @@ public class PlayerSystem : MonoBehaviour
 {
     private Vector2 direction;
     [SerializeField] private float walkSpeed;
+    [SerializeField] private Transform targetCamera;
+    private PlayerRole role;
+    private int ID;
+
+    private void Start()
+    {
+        targetCamera.parent = null;
+        GameManager.instance.AddPlayer(this);
+    }
+
+    public void Setup(PlayerRole myRole, int myID)
+    {
+        Debug.Log(role + ", " + ID);
+        role = myRole;
+        ID = myID;
+    }
 
     private void Update()
     {
