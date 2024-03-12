13f45d393ae4994f59e16a1219923adf06befd08
mandarinCapybara <mitchelgeraedts@gmail.com>
2024-03-12 18:57:38 +0100

    Camera script added

---
+++ Context1/Assets/Scripts/PlayerManager/CameraLogic.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLogic : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField] private float chaseSpeed = 5;
+    
+    private void Awake()
+    {
+        target = transform.parent;
+    }
+
+    void Update()
+    {
+        if(target != null)
+        {
+            Vector3 targetPos = new(target.position.x, target.position.y, transform.position.z); // keep own z offset cuz 2D camera logic
+
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, chaseSpeed);
+        }
+    }
+}
