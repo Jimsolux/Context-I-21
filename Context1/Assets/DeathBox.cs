42375bc2ecc02fa6e97a22577e3592a88fe0f9bf
Jim Dekker <jim.dekker@outlook.com>
2024-03-21 15:23:05 +0100

    JimslevelDesign :)

--- Context1/Assets/DeathBox.cs
+++ Context1/Assets/DeathBox.cs
@@ -7,6 +7,7 @@ public class DeathBox : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        other.GetComponent<PlayerSystem>().Die();
+        other.TryGetComponent<PlayerSystem>(out PlayerSystem system);
+        if (system != null) { system.Die(); }
     }
 }
