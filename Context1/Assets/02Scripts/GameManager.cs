90ce7c2375c3e425bd8b53183e917ac471b10964
mandarinCapybara <mitchelgeraedts@gmail.com>
2024-03-22 15:25:17 +0100

    oui oui, ui is finish

--- Context1/Assets/02Scripts/GameManager.cs
+++ Context1/Assets/02Scripts/GameManager.cs
@@ -34,9 +34,12 @@ public class GameManager : MonoBehaviour
     }
     private void Start()
     {
-        Camera.main.gameObject.SetActive(false);
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        foreach(GameObject g in players)
+
+        if(players.Length > 0)
+            Camera.main.gameObject.SetActive(false);
+
+        foreach (GameObject g in players)
         {
             g.GetComponent<PlayerSystem>().Die();
         }
