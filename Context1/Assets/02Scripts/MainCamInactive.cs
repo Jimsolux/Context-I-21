11fbd538a7a89b62a33433918a7288d3162df228
mandarinCapybara <mitchelgeraedts@gmail.com>
2024-03-22 17:23:38 +0100

    Bug fix i hope maybe

---
+++ Context1/Assets/02Scripts/MainCamInactive.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamInactive : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+}
